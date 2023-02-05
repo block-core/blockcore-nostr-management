a570e06626660ab917214adfa602bd452d099a9d
missmeowness <lbarvinskayaa@gmail.com>
2023-02-05 16:02:58 +0300

    Add connect button

--- src/app/connect/connect.ts
+++ src/app/connect/connect.ts
@@ -1,10 +1,23 @@
 import { ChangeDetectorRef, NgZone } from '@angular/core';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication';
 
 @Component({
   selector: 'app-connect',
   templateUrl: './connect.html',
   styleUrls: ['./connect.css'],
 })
-export class ConnectComponent {}
+export class ConnectComponent {
+  constructor(
+    private authService: AuthenticationService, 
+    private router: Router) {}
+
+  async connect() {
+    const userInfo = await this.authService.login();
+
+    if (userInfo.authenticated()) {
+      this.router.navigateByUrl('/');
+    }
+  }
+}
